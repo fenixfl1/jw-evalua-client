0a047c5b0df78280fb97a93fdad1f83fbfb41635
Benjamin Rosario <benjaminfl119@gmail.com>
2025-10-17 14:30:07 -0400

    feat: update Template component to use dynamic background based on theme

--- src/pages/reset_password/template.tsx
+++ src/pages/reset_password/template.tsx
@@ -1,19 +1,27 @@
 import React from 'react'
+import { useAppContext } from 'src/context/AppContext'
 import styled from 'styled-components'
 
-const Container = styled.div`
+const Container = styled.div<{ isDark: boolean }>`
   height: 100vh;
   width: 100vw;
   display: flex;
   justify-content: center;
   align-items: center;
-  background-image: url('assets/main-background.webp');
-  background-repeat: no-repeat;
-  background-size: cover;
+  background: ${({ isDark }) =>
+    isDark
+      ? '#333'
+      : `radial-gradient(
+    circle at 30% 30%,
+    #ffe0ec,
+    #e9f7ff 50%,
+    #eaf7ef 100%
+  )`};
 `
 
-const template: React.FC<React.PropsWithChildren> = ({ children }) => {
-  return <Container>{children}</Container>
+const Template: React.FC<React.PropsWithChildren> = ({ children }) => {
+  const { theme } = useAppContext()
+  return <Container isDark={theme === 'dark'}>{children}</Container>
 }
 
-export default template
+export default Template
